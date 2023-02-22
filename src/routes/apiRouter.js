27441b6dda928fc4682018a948c1811881b2cb44
carla <carlajilo@gmail.com>
2023-02-22 20:01:04 +0100

    cortesRouter

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -7,8 +7,10 @@ const apirouter = require("express").Router();
 
 const tecnicorouter = require("./tecnicoRouter");
 const casetterouter = require("./casetteRouter");
+const corterouter = require("./cortesRouter");
 
 apirouter.use("/tecnicos", tecnicorouter);
 apirouter.use("/casettes", casetterouter);
+apirouter.use("/cortes", corterouter);
 
 module.exports = apirouter;
