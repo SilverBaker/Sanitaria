30083f362ec7795de9b4c5f4e470f71e87703d8c
SilverBaker <silverbaker@outlook.es>
2023-03-08 19:20:42 +0100

    Added cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+const cors=require("cors")
 
 require("./database/associations")
 
@@ -10,12 +11,16 @@ const apiroutes = require("./routes/apiRouter");
 // importacion de la conexion/instancia de la base de datos
 const sequelize=require("./database/db")
 
-// Conversión a json datos que nos envína para post, put, patch...
+// Conversión a json datos que nos envían para post, put, patch...
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 // conexión con mysql
 app.use("/v1", apiroutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- src/services/TecnicoService.js
+++ src/services/TecnicoService.js
@@ -3,16 +3,25 @@ const bcrypt = require("bcryptjs")
 const moment = require("moment")
 const jwt = require("jwt-simple")
 
+/**
+ * Recibe email como parámetro y se encarga de devolver el usuario de la base de datos con ese email
+ * 
+ * 
+ * @param {String} email 
+ * @returns {JSON}
+ */
 const getUnTecnicoMail = async (email) => {
     const tecnico = await Tecnico.findOne({ where: { email: `${email}` } })
-    return tecnico
-        ;
+    return tecnico;
 }
+
+
 
 const getAllTecnicos = async () => {
     const tecnicos = await Tecnico.findAll();
     return tecnicos
 }
+
 
 
 const createToken = (user) => {
@@ -24,6 +33,13 @@ const createToken = (user) => {
     return jwt.encode(payload, "Token256")
 }
 
+
+/**
+ * Recibe
+ * 
+ * @param {JSON} req 
+ * @returns 
+ */
 const login = async (req) => {
     const user = await Tecnico.findOne({
         where: {
