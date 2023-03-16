52e580135479da1f9f64766f9000d0998773f460
SilverBaker <silverbaker@outlook.es>
2023-03-16 16:12:25 +0100

    Comentado require de mailer

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const mailer = require("./email/sendEmail");
+// require("./email/sendEmail");
 
 require("./database/associations")
 
